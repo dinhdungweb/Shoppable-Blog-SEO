a8fb131a7e7d3504b5726514191c7919d45ff47c
Dinhdungweb <dungmaster7@gmail.com>
2026-06-30 20:38:58 +0700

    Add privacy compliance webhooks

---
+++ app/routes/webhooks.shop.redact.tsx
@@ -0,0 +1,11 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { shop, payload, topic } = await authenticate.webhook(request);
+  console.log(`Received ${topic} webhook for ${shop}`);
+  
+  // Payload contains shop deletion request details
+  
+  return new Response();
+};
